e85af48e981531be457c99c95553252802080dfb
runcken <runcken@gmail.com>
2026-09-19 14:07:31 +0300

    configuring startup via Docker

--- star_burger/urls.py
+++ star_burger/urls.py
@@ -1,9 +1,9 @@
+from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import render
 
-from . import settings
 from foodcartapp import views
 
 urlpatterns = [
@@ -13,10 +13,12 @@ urlpatterns = [
     path('api/', include('foodcartapp.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('manager/', include('restaurateur.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
 if settings.DEBUG:
     import debug_toolbar
-    urlpatterns = [
-        path(r'__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
